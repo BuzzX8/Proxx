5705433dce28ae911ec261ee0badf8909f49f1c2
Maxim <maxim.chasnik@gmail.com>
2023-02-16 16:59:33 +0200

    Added adjectancy test

--- ProxxLib.Tests/BoardTests.cs
+++ ProxxLib.Tests/BoardTests.cs
@@ -3,9 +3,22 @@ namespace ProxxLib.Tests
     public class BoardTests
     {
         [Fact]
-        public void Test1()
+        public void Constructor_Correctly_Builds_Adjectent_Cells_For_Corner_Cell()
         {
+            var cellValues = new int?[] 
+            {
+                1, 2, 3, 
+                4, 5, 6, 
+                7, 8, 9 
+            };
+            var board = new Board(cellValues);
 
+            var cell = board[0, 0];
+
+            Assert.Equal(3, cell.AdjacentCells.Count);
+            Assert.Contains(board[1, 0], cell.AdjacentCells);
+            Assert.Contains(board[1, 1], cell.AdjacentCells);
+            Assert.Contains(board[0, 1], cell.AdjacentCells);
         }
     }
 }
--- ProxxLib/Board.cs
+++ ProxxLib/Board.cs
@@ -4,10 +4,52 @@
     {
         public Board(IEnumerable<int?> cellValues)
         {
+            Size = (int)Math.Sqrt(cellValues.Count());
             Cells = cellValues.Select(value => new Cell { Value = value }).ToArray();
+            BuildAdjectentCells();
         }
 
-        public IReadOnlyCollection<Cell> Cells { get; set; }
+        private void BuildAdjectentCells()
+        {
+            var adjectencyIndicies = new[] 
+            { 
+                (-1, -1), (-1, 0), (-1, 1), 
+                (0, -1), (0, 1),
+                (1, -1), (1, 0), (1, 1)
+            };
+
+            for (int i = 0; i < Cells.Count; i++)
+            {
+                var column = i / Size;
+                var row = i % Size;
+                var cell = Cells[i];
+
+                foreach(var adjIdx in adjectencyIndicies)
+                {
+                    var adjColumn = column + adjIdx.Item1;
+                    var adjRow = row + adjIdx.Item2;
+
+                    if (adjColumn < 0 || adjColumn >= Size || adjRow < 0 || adjRow >= Size)
+                    {
+                        continue;
+                    }
+
+                    cell.AdjacentCells.Add(this[adjColumn, adjRow]);
+                }
+            }
+        }
+
+        public Cell this[int columnm, int row]
+        {
+            get
+            {
+                var index = Size * columnm + row;
+
+                return Cells[index];
+            }
+        }
+
+        public IReadOnlyList<Cell> Cells { get; set; }
 
         public GameState GameState
         {
@@ -26,6 +68,8 @@
                 return GameState.InProgress;
             }
         }
+
+        public int Size { get; }
 
         public void OpenCell(int column, int row)
         {
